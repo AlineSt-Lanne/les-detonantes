e8241d7fc5703bd03aa6b3d6ef89c8b2cd589213
Aline Saint-Lanne <aline.saint-lanne@orange.fr>
2022-07-22 14:06:58 +0200

    maj database

--- backend/database.sql
+++ backend/database.sql
@@ -25,3 +25,7 @@ INSERT INTO `sponsors` (image, name, category) VALUES
 ("https://postimg.cc/r0xJDs32", "Decathlon", "Sport"),
 ("https://postimg.cc/y36v2Bpq", "Nantes", "Ville"),
 ("https://postimg.cc/S2krzkhX", "Télénantes", "Audiovisuelle");
+
+INSERT INTO `admin` (pseudo, password, role) VALUES
+("Vicky", "$2b$10$uATpkS2uDrBZsey2TevZzeCu1kFjoKv7rwypcWctHl34rszrZ/c16", "ROLE_ADMIN"),
+("Paulette", "$2b$10$jBFInQ5r.UQaowYpWfxSteByeU/aF0A3b8zKSB7ZIbvZ6P3YUD2ZG", "ROLE_ADMIN");
